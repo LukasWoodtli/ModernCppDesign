a1dc30a6c78d9f47fc9871ffc77bba8869e792de
Lukas Woodtli <woodtli.lukas@gmail.com>
2017-04-01 10:44:40 +0200

    Add example for Type2Int

---
+++ 02_Techniques/Int2Type.cpp
@@ -0,0 +1,60 @@
+
+#include <iostream>
+
+
+template <int v>
+struct Int2Type {
+  enum {value = v};
+};
+
+class MyClass {
+};
+
+
+class MyPolymorphicClass {
+public:
+    MyPolymorphicClass* Clone() {return NULL;}
+};
+
+template <typename T, bool isPolymorphic>
+class NiftyContainer {
+private:
+  void DoSomething(T* pObj, Int2Type<true>) {
+    T* pNewObject = pObj->Clone();
+    // ... polymorphic algorithm ...
+    (void)pNewObject;
+  }
+
+  void DoSomething(T* pObj, Int2Type<false>) {
+    T* pNewObj = new T(*pObj);
+    // ... non-polymorphic algorithm ...
+    (void)pNewObj;
+  }
+
+public:
+  // ...
+  void DoSomething() {
+    T* pSomeObj = new T;
+    DoSomething(pSomeObj, Int2Type<isPolymorphic>());
+    (void)pSomeObj;
+  }
+};
+
+
+
+int main(void) {
+
+  NiftyContainer<MyClass, false> myClass;
+  myClass.DoSomething();
+
+  /* This doesn't work. No Clone method available
+  NiftyContainer<MyClass, true> myClass2;
+  myClass2.DoSomething(); */
+
+  NiftyContainer<MyPolymorphicClass, true> myPolymorphicClass;
+  myPolymorphicClass.DoSomething();
+
+
+
+  return 0;
+}
