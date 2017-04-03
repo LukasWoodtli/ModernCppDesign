ab58542e3490f3a15392030bf5306208a75aa52a
Lukas Woodtli <woodtli.lukas@gmail.com>
2017-04-03 21:48:44 +0200

    Finish example for detecting inheritance

--- 02_Techniques/Convertibility.cpp
+++ 02_Techniques/Convertibility.cpp
@@ -12,18 +12,52 @@ class Conversion {
 
 public:
 	enum { exists = sizeof(Test(MakeT())) == sizeof(Small) };
+	enum { sameType = false };
 };
 
-// just for testing purposes
-template <typename A, typename B>
-void TEST_ASSERT(A a, B b) { if(a != b) exit(1); }
+template <class T>
+class Conversion<T, T> {
+
+public:
+	enum { exists = 1, sameType = 1  };
+};
+
+
+#define SUPERSUBCLASS(T, U) \
+	(Conversion<const U*, const T*>::exists && \
+	!Conversion<const T*, const void*>::sameType)
+
+#define SUPERSUBCLASS_STRICT(T, U) \
+	(SUPERSUBCLASS(T, U) && \
+	!Conversion<const T*, const U*>::sameType)
+
+// testing
+class A {};
+class B : public A {};
+class C : public B {};
+
+class U {};
+class V : protected U {};
+class W : private U {};
 
 int main(void) {
-	
-	TEST_ASSERT(Conversion<double, int>::exists, 1);
-	TEST_ASSERT(Conversion<char, char*>::exists, 0);
-	TEST_ASSERT(Conversion<size_t, std::vector<int>>::exists, 0);
-	
+
+	static_assert(Conversion<double, int>::exists == 1, "Types are not convertible");
+	static_assert(Conversion<char, char*>::exists == 0, "Types are not convertible");
+	static_assert(Conversion<size_t, std::vector<int>>::exists == 0, "Types are not convertible");
+
+
+	static_assert(SUPERSUBCLASS(A, B) == true, "Types are not convertible");
+	static_assert(SUPERSUBCLASS(A, C) == true, "Types are not convertible");
+
+	static_assert(SUPERSUBCLASS(A, U) == false, "Types are not convertible");
+
+	// these don't work due to nonpublic inheritance
+	//static_assert(SUPERSUBCLASS(U, V) == false, "Types are not convertible");
+	//static_assert(SUPERSUBCLASS(U, W) == false, "Types are not convertible");
+
+	static_assert(SUPERSUBCLASS(A, A) == true, "Types are not convertible");
+	static_assert(SUPERSUBCLASS_STRICT(A, A) == false, "Types are not convertible");
+
 	return 0;
 }
-
--- README.md
+++ README.md
@@ -35,8 +35,8 @@ My playground for the examples of the famous book: ["Modern C++ Design"](http://
 |  2.3    | Local Classes                       |   ✓  |
 |  2.4    | Mapping Integral Constants to Types |   ✓  |
 |  2.5    | Type-to-Type Mapping                |   ✓  |
-|  2.6    | Type Selection                      |      |
-|  2.7    | Detecting Convertibility and Inheritance at Compile Time | |
+|  2.6    | Type Selection                      |   ✓  |
+|  2.7    | Detecting Convertibility and Inheritance at Compile Time | ✓ |
 |  2.8    | A Wrapper Around type_info          |      |
 |  2.9    | NullType and EmptyType              |      |
 | 2.10    | Type Traits                         |      |
