6bb7b1a4546dc0f46b4ce538fa817232a81ec375
Lukas Woodtli <woodtli.lukas@gmail.com>
2017-05-06 22:34:43 +0200

    Work on Small Object Allocation

--- 04_SmallObjectAllocation/SmallObjectAllocation.cpp
+++ 04_SmallObjectAllocation/SmallObjectAllocation.cpp
@@ -1,5 +1,9 @@
 #include <cstddef>
 #include <iostream>
+#include <assert.h>
+#include <vector>
+
+/********* Chunks ***********************************/
 
 // POD: no ctor...
 struct Chunk {
@@ -31,6 +35,72 @@ void* Chunk::Allocate(std::size_t blockSize) {
   --blocksAvailable_;
   return pResult;
 }
+
+void Chunk::Deallocate(void* p, std::size_t blockSize) {
+  assert(p >= pData_);
+  unsigned char* toRelease = static_cast<unsigned char*>(p);
+
+  // alignment check
+  assert((toRelease - pData_) % blockSize == 0);
+
+  *toRelease = firstAvailableBlock_;
+  firstAvailableBlock_ = static_cast<unsigned char>((toRelease - pData_) / blockSize);
+
+  // truncation check
+  assert(firstAvailableBlock_ == (toRelease - pData_) / blockSize);
+  ++blocksAvailable_;
+}
+
+
+/********* Fixed-Size Allocator ***********************************/
+
+class FixedAllocator {
+public:
+// ...
+
+  void* Allocate();
+
+
+private:
+  std::size_t blockSize_;
+  unsigned char numBlocks_;
+  typedef std::vector<Chunk> Chunks;
+  Chunks chunks_;
+  Chunk* allocChunk_;
+  Chunk* deallocChunk_;
+
+};
+
+void* FixedAllocator::Allocate() {
+  if (allocChunk_ == 0 ||
+      allocChunk_->blocksAvailable_ == 0) {
+    // no available memory in chunk
+    // try to find one
+    Chunks::iterator i = chunks_.begin();
+    for (;; ++i) {
+      if (i == chunks_.end()) {
+        // all filled up - add a new chunk
+        chunks_.reserve(chunks_.size()+1);
+        Chunk newChunk;
+        newChunk.Init(blockSize_, numBlocks_);
+        chunks_.push_back(newChunk);
+        allocChunk_ = &chunks_.back();
+        deallocChunk_ = &chunks_.back();
+        break;
+      }
+      if (i->blocksAvailable_ > 0) {
+        // found a chunk
+        allocChunk_ = &*i;
+        break;
+      }
+    }
+  }
+  assert(allocChunk_ != 0);
+  assert(allocChunk_->blocksAvailable_ > 0);
+  return allocChunk_->Allocate(blockSize_);
+}
+
+
 
 int main() {
 
