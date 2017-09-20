6d0a365ab5c8d560dc2ee65b72d4b35fc2e3f574
Lukas Woodtli <woodtli.lukas@gmail.com>
2017-09-20 17:53:10 +0200

    Work on singletons

--- 06_Singleton/Singleton.cpp
+++ 06_Singleton/Singleton.cpp
@@ -1,6 +1,57 @@
 #include <cstdlib>
 #include <new>
 
+namespace Private {
+class LifetimeTracker {
+public:
+	LifetimeTracker(unsigned int x) : longevity_(x) {}
+
+	virtual ~LifetimeTracker() = 0;
+
+	friend inline bool Comare(unsigned int longevity,
+				  const LifetimeTracker* p) {
+		return p->longevity_ < longevity;
+	}
+
+private:
+	unsigned int longevity_;
+};
+
+// Definition required
+LifetimeTracker::~LifetimeTracker() {}
+
+
+
+typedef LifetimeTracker** TrackerArray;
+extern TrackerArray pTrackerArray;
+extern unsigned int elements;
+
+template <typename T>
+struct Deleter {
+    static void Delete(T* pObj) {
+        delete pObj;
+    }
+};
+
+
+template <typename T, typename Destroyer>
+class ConcreteLifetimeTracker : public LifetimeTracker {
+public:
+    ConcreteLifetimeTracker(T* p, unsigned int longevity, Destroyer d)
+        : LifetimeTracker(longevity), pTracked_(p), destroyer_(d)
+    {}
+
+    ~ConcreteLifetimeTracker() {
+        destroyer_(pTracked_);
+    }
+    
+private:
+    T* pTracked_;
+    Destroyer destroyer_;
+};
+
+}
+
 class Singleton {
 public:
     static Singleton& Instance() {
