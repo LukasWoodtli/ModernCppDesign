3a3d70295dce9fc80749447866d7714f36027cea
Lukas Woodtli <woodtli.lukas@gmail.com>
2017-11-17 20:54:52 +0100

    Work on visitor

--- 10_Visitor/Visitor.cpp
+++ 10_Visitor/Visitor.cpp
@@ -9,8 +9,12 @@ public:
     void AddChars(unsigned int charsToAdd) {
         chars_ += charsToAdd;
     }
-
-    // similar for words, images...
+    void AddWords(unsigned int charsToAdd) {
+        words_ += charsToAdd;
+    }
+    void AddImages(unsigned int charsToAdd) {
+        words_ += charsToAdd;
+    }
 
     void Display();
 };
@@ -18,6 +22,23 @@ public:
 class DocElement {
     virtual void UpdateStats(DocStats& statistics) = 0;
 };
+
+class Paragraph : public DocElement {
+public:
+    void UpdateStats(DocStats& statistics) {
+        statistics.AddChars(22); // would count number of chars
+        statistics.AddWords(3); // would count number of words
+    }
+};
+
+
+class RasterBitmap : public DocElement {
+public:
+    void UpdateStats(DocStats& statistics) {
+        statistics.AddImages(1);
+    }
+};
+
 
 int main() {
 
